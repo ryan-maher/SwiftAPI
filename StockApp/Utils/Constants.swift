167f8f9448d172dd3442e02b4ad6008f8986a268
Ryan Maher <maher.ry@northeastern.edu>
2023-10-21 17:09:37 -0700

    First commit

---
+++ StockApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockApp
+//
+//  Created by Ryan on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "3b01b2fd0c3c92cb27e5bbe1b111ad91"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
---
+++ StockApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by Ryan on 10/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let stockNames = ["MSFT", "TSLA", "META", "GOOG"]
+
+    @IBOutlet weak var tblView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stockNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = stockNames[indexPath.row]
+        return cell
+    }
+
+}
+
